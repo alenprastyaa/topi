cfa555fed72c59916803e7a5d045048f0359c079
alenprastya06 <alen@seleris.id>
2026-07-14 16:44:01 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const mysql = require('mysql2/promise');
 const { Sequelize, DataTypes, Op } = require('sequelize');
